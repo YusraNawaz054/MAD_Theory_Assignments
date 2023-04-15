04604f19f3f77042cfd44e5c28373d760ade1edf
YusraNawaz054 <113548080+YusraNawaz054@users.noreply.github.com>
2023-04-15 21:38:16 +0500

    Update ChessBoard.js

--- ChessBoard.js
+++ ChessBoard.js
@@ -4,18 +4,18 @@ import { View, StyleSheet } from 'react-native';
 const ChessBoard = () => {
   const board = [];
 
-  for (let row = 0; row < 8; row++) { // no. of iteration will be 8 because board is of 8x8
-    const columns = [];
+  for (let r = 0; r < 8; r++) { // no. of iteration will be 8 because board is of 8x8
+    const cols = [];
 
-    for (let col = 0; col < 8; col++) {
-      const backgroundColor = (row + col) % 2 === 0 ? 'white' : 'black'; //color of sqr wll be white if value is even otherwise black
+    for (let c = 0; c < 8; c++) {
+      const backgroundColor = (r + c) % 2 === 0 ? 'white' : 'black'; //color of sqr wll be white if value is even otherwise black
 
       
-      columns.push(
+      cols.push(
         //this View element for sqr of chessboard
         <View
         // key attribute will keep track of each sqr based on row & col
-          key={`${row},${col}`} //combines row & col numbers into a string, comma separated
+          key={`${r},${c}`} //combines row & col numbers into a string, comma separated
           style={[
             styles.square,
             { backgroundColor, width:80, height:80 } // each square of 80 width & heigth
@@ -25,7 +25,7 @@ const ChessBoard = () => {
     }
 
     // Adds each row to the chessboard
-    board.push(<View key={row} style={styles.row}>{columns}</View>);
+    board.push(<View key={r} style={styles.row}>{cols}</View>);
     //View element with key attribute based on current row number will be created
     //children of View element are columns array, which contains all the View elements for the squares in the row
 }
